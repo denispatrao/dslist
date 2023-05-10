e1d9f7c26f406854b8fde6bbd39d486b6b531e3e
Denis Patrao <denispatrao@yahoo.com.br>
2023-05-10 08:54:29 -0300

    Preenchimentos dos campos tabela jogos

--- dslist/src/main/java/com/denispatrao/dslist/entidades/Jogo.java
+++ dslist/src/main/java/com/denispatrao/dslist/entidades/Jogo.java
@@ -16,7 +16,9 @@ public class Jogo {
     private String plataforma;
     private Double pontuacao;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String descricaoCurta;
+    @Column(columnDefinition = "TEXT")
     private String descricaoLonga;
 
     public Jogo() {
